95db1b73da96f76376a82706f2829643250260b3
Joannis Orlandos <joannis@orlandos.nl>
2017-07-03 14:04:37 +0200

    codable

--- Sources/JWT/JWSCodable.swift
+++ Sources/JWT/JWSCodable.swift
@@ -1,11 +1,21 @@
 import Cheetah
 
-class JWSEncoder : Encoder {
-    public func encode(_ value: Encodable, signedBy secret: [UInt8], using header: JSONWebSignature.Header) throws -> JSONWebSignature {
-        let object = try JSONEncoder.encode(value)
+class JWSEncoder {
+    public static func encode(_ value: Encodable, signedBy secret: String, using header: JSONWebSignature.Header) throws -> JSONWebSignature {
+        return try self.encode(value, signedBy: [UInt8](secret.utf8), using: header)
+    }
+    
+    public static func encode(_ value: Encodable, signedBy secret: [UInt8], using header: JSONWebSignature.Header) throws -> JSONWebSignature {
+        let object = try JSONEncoder().encode(value )
         
         return JSONWebSignature(headers: [
             header
         ], payload: object, secret: secret)
+    }
+}
+
+class JWSDecoder {
+    public func decode<T : Decodable>(_ type: T.Type, from object: JSONWebSignature) throws -> T {
+        return try JSONDecoder().decode(type, from: object.payload)
     }
 }
