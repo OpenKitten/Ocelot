ae8cfa9116b07b1c6e4442235503304451a9c4ac
Joannis Orlandos <joannis@orlandos.nl>
2017-07-03 14:56:07 +0200

    api

--- Sources/JWT/JWS.swift
+++ Sources/JWT/JWS.swift
@@ -19,13 +19,13 @@ enum JWTError : Error {
     case unsupported
 }
 
-struct JSONWebSignature {
+public struct JSONWebSignature {
     struct JSONWebKey {
         
     }
 
-    struct Header {
-        enum Algorithm : String {
+    public struct Header {
+        public enum Algorithm : String {
             // HMAC SHA256
             case HS256
             case HS384
@@ -33,7 +33,7 @@ struct JSONWebSignature {
 
             case none
 
-            func sign(_ data: [UInt8], with secret: [UInt8]) throws -> [UInt8] {
+            internal func sign(_ data: [UInt8], with secret: [UInt8]) throws -> [UInt8] {
                 switch self {
                 case .HS256:
                     return try HMAC(key: secret, variant: .sha256).authenticate(data)
@@ -47,9 +47,9 @@ struct JSONWebSignature {
             }
         }
         
-        var alg: Algorithm
-        var signatureType: String?
-        var payloadType: String?
+        public var alg: Algorithm
+        public var signatureType: String?
+        public var payloadType: String?
 
         var jsonWebKeySetURL: String?
         var jsonWebKey: JSONWebKey?
@@ -58,12 +58,12 @@ struct JSONWebSignature {
         var criticalFields = [String]()
         var additionalFields: JSONObject
         
-        init(verifiedBy algorithm: Algorithm) {
+        public init(verifiedBy algorithm: Algorithm) {
             self.alg = algorithm
             self.additionalFields = JSONObject()
         }
 
-        init(_ object: JSONObject) throws {
+        public init(_ object: JSONObject) throws {
             var object = object
 
             guard let algName = String(object["alg"]), let alg = Algorithm(rawValue: algName) else {
@@ -97,7 +97,7 @@ struct JSONWebSignature {
             self.additionalFields = object
         }
 
-        func serializeProtectedHeader() -> JSONObject {
+        public func serializeProtectedHeader() -> JSONObject {
             return [
                 "alg": alg.rawValue,
                 "typ": signatureType,
@@ -107,37 +107,41 @@ struct JSONWebSignature {
         }
     }
 
-    var headers: [Header]
-    var payload: JSONObject
-    var secret: [UInt8]
+    public var headers: [Header]
+    public var payload: JSONObject
+    private var secret: [UInt8]
 
-    func sign(_ header: Header) throws -> [UInt8] {
-        let headerData = header.serializeProtectedHeader().serialize()
+    public func sign(_ header: Header? = nil) throws -> [UInt8] {
+        let usedHeader: Header
+        
+        if let header = header {
+            usedHeader = header
+        } else {
+            guard let header = headers.first else {
+                throw JWTError.unsupported
+            }
+            
+            usedHeader = header
+        }
+        
+        let headerData = usedHeader.serializeProtectedHeader().serialize()
         let encodedHeader = [UInt8](Base64.encode(headerData).utf8)
         let encodedPayload =  [UInt8](Base64.encode(payload.serialize()).utf8)
         
-        return encodedHeader + [0x2e] + encodedPayload + [0x2e] + [UInt8](Base64.encode(try header.alg.sign(encodedHeader + [0x2e] + encodedPayload, with: secret)).utf8)
+        return encodedHeader + [0x2e] + encodedPayload + [0x2e] + [UInt8](Base64.encode(try usedHeader.alg.sign(encodedHeader + [0x2e] + encodedPayload, with: secret)).utf8)
     }
     
-    func serializeAll() throws -> [[UInt8]] {
-        guard headers.count > 0 else {
-            throw JWTError.unsupported
-        }
-        
-        return try headers.map(sign)
-    }
-
-    init(headers: [Header], payload: JSONObject, secret: [UInt8]) {
+    public init(headers: [Header], payload: JSONObject, secret: [UInt8]) {
         self.headers = headers
         self.payload = payload
         self.secret = secret
     }
     
-    init(from string: String, verifyingWith secret: [UInt8]) throws {
+    public init(from string: String, verifyingWith secret: [UInt8]) throws {
         try self.init(from: [UInt8](string.utf8), verifyingWith: secret)
     }
 
-    init(from string: [UInt8], verifyingWith secret: [UInt8]) throws {
+    public init(from string: [UInt8], verifyingWith secret: [UInt8]) throws {
         let parts = string.split(separator: 0x2e)
 
         self.secret = secret
--- Tests/JWTTests/JWTTests.swift
+++ Tests/JWTTests/JWTTests.swift
@@ -10,11 +10,11 @@ class JWTTests: XCTestCase {
         
         let secret = [UInt8]("kaaaaaaas".utf8)
         
-        let signed = try JSONWebSignature(headers: [JSONWebSignature.Header(verifiedBy: .HS256) ], payload: json, secret: secret).serializeAll()
+        let signed = try JSONWebSignature(headers: [JSONWebSignature.Header(verifiedBy: .HS256) ], payload: json, secret: secret).sign()
         
-        print(String(bytes: signed.first!, encoding: .utf8))
+        print(String(bytes: signed, encoding: .utf8))
         
-        let signature = try JSONWebSignature(from: signed.first!, verifyingWith: secret)
+        let signature = try JSONWebSignature(from: signed, verifyingWith: secret)
         XCTAssertEqual(json, signature.payload)
     }
 
