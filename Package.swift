337a40c2476ea70ccfc582e715c62149fbb271e0
Joannis Orlandos <joannis@orlandos.nl>
2017-08-03 13:14:59 +0200

    Product

--- Package.swift
+++ Package.swift
@@ -6,6 +6,12 @@ import PackageDescription
 
 let package = Package(
     name: "Ocelot",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Ocelot",
+            targets: ["Ocelot"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/OpenKitten/Cheetah.git", .revision("framework")),
         .package(url: "https://github.com/OpenKitten/CryptoKitten.git", from: Version(0,1,0))
